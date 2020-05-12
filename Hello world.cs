adfe07dc59d3e1e6d62f3e3825eba7cd1ffc964c
yourgituser <renuverm@in.ibm.com>
2020-05-12 16:46:31 +0530

    added code to test

--- Hello world.cs
+++ Hello world.cs
@@ -1,1 +1,20 @@
-return "hello world";
+using System;
+ namespace DotnetBot{
+     public static class program {     
+             public static void main(string[] args)
+             {
+                 string message="";
+                 if(args.Length<1)
+                 {
+                     message="welcome to dot net core";
+                 }
+                 else{
+                     foreach(string items in args)
+                     {
+                         message +=item;
+                     }
+                 }
+             }
+     }
+
+ }
